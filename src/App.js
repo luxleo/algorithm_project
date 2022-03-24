b4b3eedfaa4e4039f10efb09c80622e2a7be46e9
이동한 <leedonghan@idonghan-ui-MacBookPro.local>
2022-03-25 04:41:54 +0900

    add Home component and update nav bars

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import './App.scss';
+import Home from './Home/Home';
 import RouteTracker from './RouteTracker/RouteTracker';
 import SortingBars from './SortingBars/SortingBars';
 
@@ -8,44 +9,22 @@ function App() {
 		<div className="App">
 			<header className="App-header"></header>
 			<BrowserRouter>
-				<div className="navBar">
-					<div>
-						<Link to="Home" className="navlink">
-							Home
+				<div className="navContainer">
+					<div className="navBar">
+						<Link to="/" className="navlink">
+							<div className="navDiv">Home</div>
 						</Link>
-					</div>
-					<div>
 						<Link to="route-tracker" className="navlink">
-							Route Tracker
+							<div className="navDiv">Route Tracker</div>
 						</Link>
-					</div>
-					<div>
-						<Link
-							to="sorting-bars"
-							className="navlink"
-							element={<SortingBars />}
-						>
-							Sorting Bars
+						<Link to="sorting-bars" className="navlink">
+							<div className="navDiv">Sorting Bars</div>
 						</Link>
 					</div>
 				</div>
-				<div className="card_link">
-					<div className="card">
-						<Link to="route-tracker" className="navlink">
-							Route Tracker
-						</Link>
-					</div>
-					<div className="card">
-						<Link
-							to="sorting-bars"
-							className="navlink"
-							element={<SortingBars />}
-						>
-							Sorting Bars
-						</Link>
-					</div>
-				</div>
+
 				<Routes>
+					<Route path="/" element={<Home />} />
 					<Route path="route-tracker" element={<RouteTracker />} />
 					<Route path="sorting-bars" element={<SortingBars />} />
 				</Routes>
---
+++ src/Home/Home.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import RouteTracker from '../RouteTracker/RouteTracker';
+import SortingBars from '../SortingBars/SortingBars';
+import './Home.scss';
+
+const Home = () => {
+	return (
+		<>
+			<div className="homecontainer">
+				<div className="card_link">
+					<Link to="route-tracker" className="card">
+						Route Tracker
+					</Link>
+					<Link to="sorting-bars" className="card">
+						Sorting Bars
+					</Link>
+				</div>
+			</div>
+		</>
+	);
+};
+
+export default Home;

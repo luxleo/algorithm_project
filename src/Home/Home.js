4781b941e6ea05f8d5879b4af05cbfd304067867
이동한 <leedonghan@idonghan-ui-MacBookPro.local>
2022-03-25 04:52:13 +0900

    update to build

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import RouteTracker from '../RouteTracker/RouteTracker';
 import SortingBars from '../SortingBars/SortingBars';
 import './Home.scss';
